2f872da8591df76dd92722eeeb2c38e8bd993b23
Jojaah12 <david.aljojah@gmail.com>
2023-07-19 01:50:52 +0200

    changed logo and Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Products from "@/components/Products";
+
 export default function Home() {
   return (
-    <div>
-      <h1>starting here</h1>
-    </div>
+    <main>
+      <div
+        className="flex flex-col px-40 py-40 items-start h-screen
+     text-white"
+      >
+        <h1 className="text-5xl font-medium">
+          Solenergi för en ljusare framtid
+        </h1>
+        <p className="text-lg font-medium">
+          Fånga solen, sänk din elräkning och ditt klimatavtryck
+        </p>
+      </div>
+      <div>
+        <Products />
+      </div>
+    </main>
   );
 }
