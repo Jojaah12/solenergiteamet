1e67e64d767c0b67158fa158de26c7bb3a211ea2
Jojaah12 <david.aljojah@gmail.com>
2023-07-24 04:27:08 +0200

    Changed Footer component structure and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,57 +1,203 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { footerLinks } from "../constants";
-
 const Footer = () => (
-  <footer className="flex text-black bg-[#211D1D]  mt-5 border-t border-black">
-    <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-      <div className="flex flex-col justify-start items-start gap-6">
-        <Image
-          src="/images/logo-footer.png"
-          alt="logo"
-          width={200}
-          height={100}
-          className="object-contain"
-        />
-        <p className="text-base text-black">
-          &copy; Solenergi Teamet 2023
-        </p>
+  <footer className="text-white bg-[#394648] body-font">
+    <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+      <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+        <Link
+          href="/"
+          className="flex title-font font-medium items-center md:justify-start justify-center text-white"
+        >
+          <Image
+            src="/images/logo2.png"
+            alt="Logo"
+            width={200}
+            height={150}
+            className="object-contain "
+          />
+        </Link>
       </div>
-
-      <div className="flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20">
-        {footerLinks.map((item) => (
-          <div
-            key={item.title}
-            className="flex flex-col gap-6 text-base min-w-[170px]"
-          >
-            <h3 className="font-bold">{item.title}</h3>
-            <div className="flex flex-col gap-5">
-              {item.links.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.url}
-                  className="text-white"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
+      <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+            CATEGORIES
+          </h2>
+          <nav className="list-none mb-10">
+            <li>
+              <a className="text-white hover:text-gray-800">
+                First Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Second Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Third Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Fourth Link
+              </a>
+            </li>
+          </nav>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+            CATEGORIES
+          </h2>
+          <nav className="list-none mb-10">
+            <li>
+              <a className="text-white hover:text-gray-800">
+                First Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Second Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Third Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Fourth Link
+              </a>
+            </li>
+          </nav>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+            CATEGORIES
+          </h2>
+          <nav className="list-none mb-10">
+            <li>
+              <a className="text-white hover:text-gray-800">
+                First Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Second Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Third Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Fourth Link
+              </a>
+            </li>
+          </nav>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+            CATEGORIES
+          </h2>
+          <nav className="list-none mb-10">
+            <li>
+              <a className="text-white hover:text-gray-800">
+                First Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Second Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Third Link
+              </a>
+            </li>
+            <li>
+              <a className="text-white hover:text-gray-800">
+                Fourth Link
+              </a>
+            </li>
+          </nav>
+        </div>
       </div>
     </div>
-
-    <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-      <p>@2023 Solenergi Teamet.</p>
-
-      <div className="flex-1 flex sm:justify-end justify-center max-sm:mt-4 gap-10">
-        <Link href="/" className="text-gray-500">
-          Privacy & Policy
-        </Link>
-        <Link href="/" className="text-gray-500">
-          Terms & Condition
-        </Link>
+    <div className="bg-[#DBA737]">
+      <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+        <p className="text-white text-sm text-center sm:text-left">
+          © 2023 Solenergi Teamet
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+          <a className="text-white">
+            <svg
+              fill="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-white">
+            <svg
+              fill="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-white">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <rect
+                width="20"
+                height="20"
+                x="2"
+                y="2"
+                rx="5"
+                ry="5"
+              ></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-white">
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="0"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="none"
+                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+              ></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+        </span>
       </div>
     </div>
   </footer>
