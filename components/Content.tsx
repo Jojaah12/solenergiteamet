51167498b63c15353d264bb3fde268bfec35df44
Jojaah12 <david.aljojah@gmail.com>
2023-07-26 18:12:12 +0200

    Content section are ready for use

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Content = () => {
   return (
@@ -7,83 +8,70 @@ const Content = () => {
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-black">
-              Pitchfork Kickstarter Taxidermy
+              Ta reda på mer om våra solcellslösningar
             </h1>
             <div className="h-1 w-20 bg-[#DBA737] rounded"></div>
           </div>
           <p className="lg:w-1/2 w-full leading-relaxed text-black">
-            Whatever cardigan tote bag tumblr hexagon brooklyn
-            asymmetrical gentrify, subway tile poke farm-to-table.
-            Franzen you probably haven&apos;t heard of them man bun
-            deep jianbing selfies heirloom prism food truck ugh squid
-            celiac humblebrag.
+            Vi är experter på solcellsinstallationer för olika typer
+            av fastigheter, såsom villor, företag och
+            bostadsrättsföreningar. Välj det område som intresserar
+            dig och upptäck dess unika fördelar.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
           <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <Image
-                src="/images/content-1.jpg"
-                alt="content"
-                width={200}
-                height={150}
-                className="rounded w-full object-contain mb-6"
-              />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Chichen Itza
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat.
-                Distillery hexagon disrupt edison bulbche.
-              </p>
-            </div>
+            <Link href="/villa">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <Image
+                  src="/images/content-1.jpg"
+                  alt="content"
+                  width={200}
+                  height={150}
+                  className="rounded w-full object-contain mb-6"
+                />
+                <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Villa
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Funderar du på att montera solpaneler på din villa?
+                  Vi är här för att hjälpa dig!
+                </p>
+              </div>
+            </Link>
+          </div>
+
+          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <Link href="/företag">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <Image
+                  src="/images/content-3.jpg"
+                  alt="content"
+                  width={200}
+                  height={150}
+                  className="rounded w-full object-contain mb-6"
+                />
+                <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Företag
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Funderar ditt företag på solpaneler? Vi är experter
+                  på solenergilösningar för företag. Minska
+                  energikostnader och bli mer miljövänliga med våra
+                  skräddarsydda solpanelssystem. Kontakta oss för en
+                  konsultation idag!
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <Image
-                src="/images/content-2.jpg"
-                alt="content"
-                width={200}
-                height={150}
-                className="rounded w-full object-contain mb-6"
-              />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Colosseum Roma
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat.
-                Distillery hexagon disrupt edison bulbche.
-              </p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <Image
-                src="/images/content-3.jpg"
-                alt="content"
-                width={200}
-                height={150}
-                className="rounded w-full object-contain mb-6"
-              />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Great Pyramid of Giza
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat.
-                Distillery hexagon disrupt edison bulbche.
-              </p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
+            <Link href="/bostadsrättsföreningar"></Link>
             <div className="bg-gray-100 p-6 rounded-lg">
               <Image
                 src="/images/content-4.jpg"
@@ -92,15 +80,16 @@ const Content = () => {
                 height={150}
                 className="rounded w-full object-contain mb-6"
               />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+              <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
                 SUBTITLE
               </h3>
               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                San Francisco
+                Bostadsrättsföreningar
               </h2>
               <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat.
-                Distillery hexagon disrupt edison bulbche.
+                Optimera er bostadsrättsförening med solenergi! Minska
+                kostnader och bli mer hållbara. Kontakta oss nu för
+                skräddarsydda solpanelslösningar.
               </p>
             </div>
           </div>
