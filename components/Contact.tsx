d40c1ffba0176d77c10a75ab5c490e97b5c33664
Jojaah12 <david.aljojah@gmail.com>
2023-07-28 23:52:59 +0200

    Small changes added to the contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <section className="text-gray-600 body-font relative">
+    <section className="text-gray-600 bg-gray-300 body-font min-h-screen relative">
       <form>
-        <div className="absolute inset-0 bg-gray-300"></div>
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
@@ -18,12 +17,11 @@ const Contact = () => {
               Default select
             </label>
             <select
-              id="default"
-              className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              data-te-select-init
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option>Kundtyp</option>
-              <option value="Privat">Privat</option>
-              <option value="Företag">Företag</option>
+              <option value="1">Privat</option>
+              <option value="2">Företag</option>
             </select>
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
@@ -38,24 +36,24 @@ const Contact = () => {
             </div>
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
-                Email
+                Adress
               </label>
               <input
-                type="email"
-                id="email"
-                name="email"
+                type="text"
+                id="address"
+                name="address"
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
-                Email
+                postnummer
               </label>
               <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                type="number"
+                id="zip"
+                name="zip"
+                className="remove-arrow w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
@@ -72,7 +70,7 @@ const Contact = () => {
 
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
-                Message
+                Meddelande
               </label>
               <textarea
                 id="message"
