89a2566ec9602a18267b38e0245c00b8e89d1dba
Jojaah12 <david.aljojah@gmail.com>
2023-07-29 15:01:14 +0200

    added small changes to nav menu and contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -47,7 +47,7 @@ const Contact = () => {
             </div>
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
-                postnummer
+                Postnummer
               </label>
               <input
                 type="number"
@@ -58,7 +58,7 @@ const Contact = () => {
             </div>
             <div className="relative mb-4">
               <label className="leading-7 text-sm text-gray-600">
-                Email
+                E-post
               </label>
               <input
                 type="email"
@@ -78,13 +78,35 @@ const Contact = () => {
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               ></textarea>
             </div>
-            <button className="text-white bg-[#DBA737] border-0 py-2 px-6 focus:outline-none hover:bg-[#B78C26] rounded text-lg">
+
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
               Skicka
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
             </button>
             <p className="text-xs text-gray-500 mt-3">
               Genom att klicka på “Skicka” bekräftar jag att jag läst
+              <br />
               <Link href="/">
-                <span></span>
+                <span className="flex text-black underline">
+                  Solenergi Teamet Integritetspolicy
+                </span>
               </Link>
             </p>
           </div>
