1295630644ecd9b0238deb7e392d93ededd19c95
Jojaah12 <david.aljojah@gmail.com>
2023-07-30 21:28:43 +0200

    The design for the contact page is done now only the logic for the submit is left

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,116 +2,164 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <section className="text-gray-600 bg-gray-300 body-font min-h-screen relative">
-      <form>
-        <div className="container px-5 py-24 mx-auto flex">
-          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Boka konsultation
-            </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">
-              Fyll i formuläret nedan för att boka in en kostnadsfri
-              konsultation med oss.
-            </p>
-            <label className="relative mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Default select
-            </label>
-            <select
-              data-te-select-init
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              <option value="1">Privat</option>
-              <option value="2">Företag</option>
-            </select>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Ditt namn
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Adress
-              </label>
-              <input
-                type="text"
-                id="address"
-                name="address"
-                className="w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Postnummer
-              </label>
-              <input
-                type="number"
-                id="zip"
-                name="zip"
-                className="remove-arrow w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                E-post
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-white rounded border border-gray-300 focus:border-[#DBA737] focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
+    <section className="text-gray-600 bg-gray-300 body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Boka konsultation
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Fyll i formuläret nedan för att boka in en kostnadsfri
+            konsultation med oss.
+          </p>
+        </div>
 
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Meddelande
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
-            </div>
-
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Skicka
-              <svg
-                className="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Förnamn
+                </label>
+                <input
+                  type="text"
+                  id="zip"
+                  name="zip"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
-              </svg>
-            </button>
-            <p className="text-xs text-gray-500 mt-3">
-              Genom att klicka på “Skicka” bekräftar jag att jag läst
-              <br />
-              <Link href="/">
-                <span className="flex text-black underline">
-                  Solenergi Teamet Integritetspolicy
-                </span>
-              </Link>
-            </p>
+              </div>
+            </div>
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Efternamn
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  E-post
+                </label>
+                <input
+                  type="text"
+                  id="zip"
+                  name="zip"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Telefonnummer
+                </label>
+                <input
+                  placeholder="+4673 123 12 31"
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Adress
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Postnummer
+                </label>
+                <input
+                  type="text"
+                  id="zip"
+                  name="zip"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full sm:w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Ort
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Meddelande
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <button
+                type="button"
+                className="text-white bg-[#DBA737] hover:bg-[#B78C26] focus:ring-4 
+              focus:outline-none focus:ring-[#FFD700] font-semibold rounded-lg text-medium px-5 
+              py-2.5 text-center inline-flex items-center justify-center 
+              dark:bg-[#B78C26] "
+              >
+                Skicka
+                <svg
+                  className="w-3.5 h-3.5 ml-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </button>
+              <p className="text-xs text-gray-500 mt-3">
+                Genom att klicka på “Skicka” bekräftar jag att jag
+                läst
+                <br />
+                <Link href="/">
+                  <span className="flex text-black underline">
+                    Solenergi Teamet Integritetspolicy
+                  </span>
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
-      </form>
+      </div>
     </section>
   );
 };
