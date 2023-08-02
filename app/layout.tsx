05af6b8c25b2b601bfd4bf38b8132d678b3db45e
Jojaah12 <david.aljojah@gmail.com>
2023-08-02 14:06:12 +0200

    removed unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
 
 const urbanist = Inter({ subsets: ["latin"] });
 
