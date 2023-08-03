bdf002d6eca95b09877b65b7852b63550258998e
Jojaah12 <david.aljojah@gmail.com>
2023-08-03 15:54:57 +0200

    changed background video for testing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,13 +8,13 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="fixed top-0 left-0 w-full h-screen object-cover z-[-1]"
+        className="fixed top-0 left-0 w-full h-screen object-cover z-[-1] opacity-90"
       >
-        <source src="/images/intro1.mp4" type="video/mp4" />
+        <source src="/images/intro4.mp4" type="video/mp4" />
       </video>
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl mb-4 font-bold text-white">
+          <h1 className="sm:text-4xl mb-4 font-semibold text-white">
             Solceller & Takbyte, snyggt och enkelt
             <br className="hidden lg:inline-block" />
             med Solenergi Teamet
