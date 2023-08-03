e94c5e709d57c1cf7fef66db10a61c85b2b2e25c
Jojaah12 <david.aljojah@gmail.com>
2023-08-03 14:50:12 +0200

    Created a new component to reuse aos lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import Contact from "@/components/Contact";
 import Content from "@/components/Content";
 import Hero from "@/components/Hero";
 import Step from "@/components/Step";
 import Testimonials from "@/components/Testimonial";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
   return (
     <main>
       <Hero />
