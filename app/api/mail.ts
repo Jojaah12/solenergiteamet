e8748eaa6eb5598e1a8876d668a733615bb03475
David Jojah <68277458+Jojaah12@users.noreply.github.com>
2023-08-15 15:08:01 +0200

    mail logic to handle sendgrid is added now

---
+++ app/api/mail.ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+type Data = {
+  message: string;
+};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "POST") {
+    const {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      address,
+      zipcode,
+      city,
+      message,
+    }: {
+      firstName: string;
+      lastName: string;
+      email: string;
+      phoneNumber: string;
+      address: string;
+      zipcode: string;
+      city: string;
+      message: string;
+    } = req.body;
+
+    const recipients: string[] = [
+      "david.aljojah@gmail.com",
+      "mohammed_jojah@hotmail.com",
+    ];
+
+    const msg = `
+      Förnamn: ${firstName}
+      Efternamn: ${lastName}
+      E-post: ${email}
+      Telefonnummer: ${phoneNumber}
+      Adress: ${address}
+      Postnummer: ${zipcode}
+      Ort: ${city}
+      Meddelande: ${message}
+    `;
+
+    const data = {
+      to: recipients,
+      from: "samirdanun@hotmail.com",
+      subject: `${firstName.toUpperCase()} har skickat meddelande från solenergiteamet.se`,
+      text: `Email => ${email}`,
+      html: msg.replace(/\n/g, "<br>"),
+    };
+    try {
+      await sgMail.send(data);
+      res
+        .status(200)
+        .json({ message: "Your message was sent successfully." });
+    } catch (err) {
+      res.status(500).json({
+        message: `There was an error sending your message. ${err}`,
+      });
+    }
+  }
+}
