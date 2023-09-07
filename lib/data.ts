e009d4b9c52285627eb0bcb9d62e806861711ab5
FredoSalat <fredrik.marthinsens@gmail.com>
2023-09-07 10:50:35 +0200

    image change

--- lib/data.ts
+++ lib/data.ts
@@ -19,6 +19,6 @@ export const takbyteData: TakbyteData[] = [
     blockTitle: "Hur fungerar taktintegrerade solceller?",
     blockText:
       "Det kan många gånger vara lönsamt att montera solceller på villan i samband med att man lägger ett nytt tak. Lönsamheten är olika hög för olika tak och beror till stor del på takets lutning och väderstreck – därför är det viktigt att ta reda på när det lönar sig för dig att installera solceller på taket innan du bestämmer dig. Det gäller oavsett om du har funderingar på att installera ett så kallat solcellstak ett tak med integrerade solceller, eller traditionella utanpåliggande solpaneler.",
-    blockImage: "/images/takbyte4.jpg",
+    blockImage: "/images/takbyte.jpg",
   },
 ];
