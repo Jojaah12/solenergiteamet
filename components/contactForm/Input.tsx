5f7464419f8c0b1fa7805b896a2b696a734cbefd
David Jojah <68277458+Jojaah12@users.noreply.github.com>
2023-09-22 21:20:36 +0200

    design update

--- components/contactForm/Input.tsx
+++ components/contactForm/Input.tsx
@@ -35,7 +35,7 @@ const Input: React.FC<InputProps> = ({
           onChange={onChange}
           className={`w-full bg-gray-100 bg-opacity-50 rounded border ${
             error ? "border-red-500" : "border-gray-300"
-          } focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`}
+          } focus:border-[#FFD700] focus:bg-white focus:ring-2 focus:ring-[#FFD700] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`}
         />
         {error && (
           <p className="text-xs text-red-500 mt-1">{error}</p>
