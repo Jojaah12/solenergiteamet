91397f3e5cf61ad1669725084b6dd205a75fb5e3
FredoSalat <fredrik.marthinsens@gmail.com>
2023-10-06 19:25:40 +0200

    Constants file created, copy for hero and takbyte externalized and made immutible

---
+++ lib/constants.ts
@@ -0,0 +1,27 @@
+export const HERODATA: { HEADING: string; SUBHEADING: string } = Object.freeze({
+  HEADING: "Solceller & Takbyte",
+  SUBHEADING:
+    "Vi är specialister på solcellslösningar och erbjuder en helhetslösning för solel. Vår engagerade personal arbetar internt för att säkerställa kontroll över hela processen. Du kan känna dig trygg med att köpa din lösning av oss.",
+});
+
+export const TAKBYTEDATA: readonly {
+  readonly ID: number;
+  readonly BLOCKTITLE: string;
+  readonly BLOCKTEXT: string;
+  readonly BLOCKIMAGE: string;
+}[] = Object.freeze([
+  {
+    ID: 1,
+    BLOCKTITLE: "Hur fungerar taktintegrerade solceller?",
+    BLOCKTEXT:
+      "Det kan många gånger vara lönsamt att montera solceller på villan i samband med att man lägger ett nytt tak...",
+    BLOCKIMAGE: "/images/green.jpg",
+  },
+  {
+    ID: 2,
+    BLOCKTITLE: "Hur fungerar taktintegrerade solceller?",
+    BLOCKTEXT:
+      "Det kan många gånger vara lönsamt att montera solceller på villan i samband med att man lägger ett nytt tak...",
+    BLOCKIMAGE: "/images/green.jpg",
+  },
+]);
