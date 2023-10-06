3a39eded67fe35d1131a9ab2935926ea782c84bd
FredoSalat <fredrik.marthinsens@gmail.com>
2023-10-06 20:05:35 +0200

    added constants to content and refactored the component to make it more dynamic

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { CONTENTCARDDATA, CONTENTDATA } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,226 +7,47 @@ const Content = () => {
     <>
       <section className="text-gray-600 bg-white body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div
-            className="flex flex-wrap w-full mb-20"
-            data-aos="fade-up"
-          >
+          <div className="flex flex-wrap w-full mb-20" data-aos="fade-up">
             <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-black">
-                Ta reda på mer om våra solcellslösningar
+                {CONTENTDATA.HEADING}
               </h1>
               <div className="h-1 w-20 bg-[#DBA737] rounded"></div>
             </div>
             <p className="lg:w-1/2 w-full leading-relaxed text-black">
-              Vi är experter på solcellsinstallationer & Takbyte för
-              olika typer av fastigheter, såsom villor, företag och
-              bostadsrättsföreningar. Välj det område som intresserar
-              dig och upptäck dess unika fördelar.
+              {CONTENTDATA.SUBHEADING}
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-1.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Villa
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er villa med solenergi! Minska kostnader
-                    och bli mer hållbara. Kontakta oss nu för
-                    skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-3.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Företag
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er företag med solenergi! Minska
-                    kostnader och bli mer hållbara. Kontakta oss nu
-                    för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/takbyte">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/takbyte1.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Takbyte
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er företag med solenergi! Minska
-                    kostnader och bli mer hållbara. Kontakta oss nu
-                    för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-4.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Bostadsrättsföreningar
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er bostadsrättsförening med solenergi!
-                    Minska kostnader och bli mer hållbara. Kontakta
-                    oss nu för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-1.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Villa
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er villa med solenergi! Minska kostnader
-                    och bli mer hållbara. Kontakta oss nu för
-                    skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-3.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Företag
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er företag med solenergi! Minska
-                    kostnader och bli mer hållbara. Kontakta oss nu
-                    för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/takbyte">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/takbyte1.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Takbyte
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er företag med solenergi! Minska
-                    kostnader och bli mer hållbara. Kontakta oss nu
-                    för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="xl:w-1/4 md:w-1/2 p-4" data-aos="fade-up">
-              <Link href="/">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image
-                    src="/images/content-4.jpg"
-                    alt="content"
-                    width={200}
-                    height={150}
-                    className="rounded w-full object-contain mb-6"
-                    data-aos="flip-left"
-                  />
-                  <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
-                    SUBTITLE
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    Bostadsrättsföreningar
-                  </h2>
-                  <p className="leading-relaxed text-base line-clamp-3">
-                    Optimera er bostadsrättsförening med solenergi!
-                    Minska kostnader och bli mer hållbara. Kontakta
-                    oss nu för skräddarsydda solpanelslösningar.
-                  </p>
-                </div>
-              </Link>
-            </div>
+            {CONTENTCARDDATA.map((content) => (
+              <div
+                key={content.ID}
+                className="xl:w-1/4 md:w-1/2 p-4"
+                data-aos="fade-up"
+              >
+                <Link href={content.HREF}>
+                  <div className="bg-gray-100 p-6 rounded-lg">
+                    <Image
+                      src={content.CARDIMAGE}
+                      alt={content.CARDTITLE}
+                      width={200}
+                      height={150}
+                      className="rounded w-full object-contain mb-6"
+                      data-aos="flip-left"
+                    />
+                    <h3 className="tracking-widest text-[#DBA737] text-xs font-medium title-font">
+                      {content.CARDSUBTITLE}
+                    </h3>
+                    <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                      {content.CARDTITLE}
+                    </h2>
+                    <p className="leading-relaxed text-base line-clamp-3">
+                      {content.CARDTEXT}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </section>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,63 @@
+import { randomUUID } from "crypto";
+
 export const HERODATA: { HEADING: string; SUBHEADING: string } = Object.freeze({
   HEADING: "Solceller & Takbyte",
   SUBHEADING:
     "Vi är specialister på solcellslösningar och erbjuder en helhetslösning för solel. Vår engagerade personal arbetar internt för att säkerställa kontroll över hela processen. Du kan känna dig trygg med att köpa din lösning av oss.",
 });
+
+export const CONTENTDATA: { HEADING: string; SUBHEADING: string } =
+  Object.freeze({
+    HEADING: "Ta reda på mer om våra solcellslösningar",
+    SUBHEADING:
+      "Vi är experter på solcellsinstallationer & Takbyte för olika typer av fastigheter, såsom villor, företag och bostadsrättsföreningar. Välj det område som intresserar dig och upptäck dess unika fördelar.",
+  });
+
+export const CONTENTCARDDATA: readonly {
+  readonly ID: number;
+  readonly CARDTITLE: string;
+  readonly CARDSUBTITLE: string;
+  readonly CARDTEXT: string;
+  readonly CARDIMAGE: string;
+  readonly HREF: string;
+}[] = Object.freeze([
+  {
+    ID: 1,
+    CARDTITLE: "Villa",
+    CARDSUBTITLE: "SUBTITLE",
+    CARDTEXT:
+      "Optimera er villa med solenergi! Minska kostnader och bli mer hållbara. Kontakta oss nu för skräddarsydda solpanelslösningar.",
+    CARDIMAGE: "/images/content-1.jpg",
+    HREF: "/",
+  },
+  {
+    ID: 2,
+    CARDTITLE: "Företag",
+    CARDSUBTITLE: "SUBTITLE",
+    CARDTEXT:
+      "Optimera er villa med solenergi! Minska kostnader och bli mer hållbara. Kontakta oss nu för skräddarsydda solpanelslösningar.",
+    CARDIMAGE: "/images/content-3.jpg",
+    HREF: "/",
+  },
+  {
+    ID: 3,
+    CARDTITLE: "Takbyte",
+    CARDSUBTITLE: "SUBTITLE",
+    CARDTEXT:
+      "Optimera er villa med solenergi! Minska kostnader och bli mer hållbara. Kontakta oss nu för skräddarsydda solpanelslösningar.",
+    CARDIMAGE: "/images/content-3.jpg",
+    HREF: "/takbyte",
+  },
+  {
+    ID: 4,
+    CARDTITLE: "Bostadsrättsföreningar",
+    CARDSUBTITLE: "SUBTITLE",
+    CARDTEXT:
+      "Optimera er villa med solenergi! Minska kostnader och bli mer hållbara. Kontakta oss nu för skräddarsydda solpanelslösningar.",
+    CARDIMAGE: "/images/content-3.jpg",
+    HREF: "/takbyte",
+  },
+]);
 
 export const TAKBYTEDATA: readonly {
   readonly ID: number;
