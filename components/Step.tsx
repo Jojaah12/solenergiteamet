8a8e6ef0cfa974091813b4ee5fb6dfdc3022c00e
David Jojah <68277458+Jojaah12@users.noreply.github.com>
2023-10-16 14:38:51 +0200

    Update step component

--- components/Step.tsx
+++ components/Step.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Step = () => {
   return (
     <div className="flex flex-wrap w-full">
-      <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6" data-aos="fade-up">
+      <div
+        className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6"
+        data-aos="fade-up"
+      >
         <div className="flex relative pb-12">
           <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
             <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
@@ -26,8 +29,9 @@ const Step = () => {
               Hitta din lösning
             </h2>
             <p className="leading-relaxed">
-              Använd vår solcellskalkylator för att se ditt potentiella sparande
-              eller fyll i formuläret för att kontakta våra experter.
+              Använd vår solcellskalkylator för att se ditt
+              potentiella sparande eller fyll i formuläret för att
+              kontakta våra experter.
             </p>
           </div>
         </div>
@@ -53,8 +57,8 @@ const Step = () => {
               Personlig rådgivning
             </h2>
             <p className="leading-relaxed">
-              Fyll i ett intresseformulär så ringer vi upp dig och bokar in ett
-              möte.
+              Fyll i ett intresseformulär så ringer vi upp dig och
+              bokar in ett möte.
             </p>
           </div>
         </div>
@@ -81,36 +85,8 @@ const Step = () => {
               Smidig installation
             </h2>
             <p className="leading-relaxed">
-              Vi monterar, besiktigar och lämnar sedan över anläggningen till
-              dig.
-            </p>
-          </div>
-        </div>
-        <div className="flex relative pb-12">
-          <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-            <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-          </div>
-          <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-          </div>
-          <div className="flex-grow pl-4" data-aos="zoom-in-right">
-            <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-              Långsiktig support
-            </h2>
-            <p className="leading-relaxed">
-              VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-              bespoke try-hard cliche palo santo offal.
+              Vi monterar, besiktigar och lämnar sedan över
+              anläggningen till dig.
             </p>
           </div>
         </div>
@@ -134,7 +110,8 @@ const Step = () => {
               Slutligen
             </h2>
             <p className="leading-relaxed">
-              Vår support finns här för dig under hela anläggningens livstid
+              Vår support finns här för dig under hela anläggningens
+              livstid
             </p>
           </div>
         </div>
