c95c89dd3b73efecc412c2d78d87084992784ee2
David Jojah <68277458+Jojaah12@users.noreply.github.com>
2023-11-04 23:33:37 +0100

    changed icons size

--- components/Step.tsx
+++ components/Step.tsx
@@ -21,7 +21,7 @@ const Step = () => {
   ];
 
   return (
-    <div className="flex flex-wrap w-full">
+    <div className="flex justify-center flex-wrap w-full">
       <div
         className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6"
         data-aos="fade-up"
@@ -30,14 +30,14 @@ const Step = () => {
           <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
             <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
           </div>
-          <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
+          <div className="flex-shrink-0 w-20 h-20 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
             <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-5 h-5"
+              className="w-10 h-10"
               viewBox="0 0 24 24"
             >
               <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
@@ -58,14 +58,14 @@ const Step = () => {
           <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
             <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
           </div>
-          <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
+          <div className="flex-shrink-0 w-20 h-20 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
             <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-5 h-5"
+              className="w-10 h-10"
               viewBox="0 0 24 24"
             >
               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -85,14 +85,14 @@ const Step = () => {
           <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
             <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
           </div>
-          <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
+          <div className="flex-shrink-0 w-20 h-20 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
             <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-5 h-5"
+              className="w-10 h-10"
               viewBox="0 0 24 24"
             >
               <circle cx="12" cy="5" r="3"></circle>
@@ -110,14 +110,14 @@ const Step = () => {
           </div>
         </div>
         <div className="flex relative">
-          <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
+          <div className="flex-shrink-0 w-20 h-20 rounded-full bg-[#DBA737] inline-flex items-center justify-center text-white relative z-10">
             <svg
               fill="none"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="w-5 h-5"
+              className="w-10 h-10"
               viewBox="0 0 24 24"
             >
               <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
