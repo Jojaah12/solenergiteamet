de0de277adb2fb23d58db5302db715174cc440bf
David Jojah <68277458+Jojaah12@users.noreply.github.com>
2024-12-17 17:42:12 +0100

    Adjust hero section hight

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,35 +1,22 @@
-"use client";
-
-import { HERODATA } from "@/constants/constants";
-
 const Hero = () => {
   return (
-    <div className="h-screen flex overflow-hidden">
+    <div className="h-[40vh] sm:h-[60vh] md:h-[65vh] flex overflow-hidden">
       <video
         autoPlay
         muted
         loop
-        className="fixed top-0 left-0 w-full h-screen object-cover z-[-1] opacity-90"
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-90"
       >
         <source src="/images/intro4.mp4" type="video/mp4" />
       </video>
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-5 py-12 md:py-16 lg:py-20 md:flex-row flex-col items-center h-full">
         <div
           className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start 
-        md:text-left mb-16 md:mb-0 items-center text-center"
+          md:text-left mb-8 md:mb-0 items-center text-center"
         >
-          <h1
-            className="text-6xl lg:w-1/2 mb-4 font-semibold text-white"
-            data-aos="fade-up"
-          >
-            {HERODATA.HEADING}
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-white">
+            Solceller & Takbyte
           </h1>
-          <p
-            className="mb-8 leading-relaxed font-medium lg:w-1/2 w-1/2 text-white"
-            data-aos="fade-up"
-          >
-            {HERODATA.SUBHEADING}
-          </p>
         </div>
       </div>
     </div>
